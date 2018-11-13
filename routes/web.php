3b5240dcbaf49cbf585fdc63a874ba85e8860724
IsisMartinez2018 <44509236+IsisMartinez2018@users.noreply.github.com>
2018-11-12 22:48:51 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,39 @@ Route::resource('profile', 'profileController');
 });
 
 
+
+
+/*
+|--------------------------------------------------------------------------
+| rutas del carrito a partir de aqui
+|
+*/
+
+
+Route::get('/shop', 'ShopController@index')->name('shop.index');
+Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
+    
+Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::post('/cart', 'CartController@store')->name('cart.store');
+
+Route::delete('/cart/{product}', 'CartController@destroy')->name('cart.destroy');
+Route::post('/cart/switchToSaveForLater/{product}', 'CartController@switchToSaveForLater')->name('cart.switchToSaveForLater');
+
+Route::delete('/saveForLater/{product}', 'SaveForLaterController@destroy')->name('saveForLater.destroy');
+Route::post('/saveForLater/switchToCart/{product}', 'SaveForLaterController@switchToCart')->name('saveForLater.switchToCart');
+
+
+
+
+
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+
+
